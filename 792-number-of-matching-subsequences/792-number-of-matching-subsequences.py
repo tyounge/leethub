d0b5cc1075d66c0dd62e88c323f7b07399be93c5
TAEYOUNG(Teo) SEO <1004tyoung@gmail.com>
2022-07-20 22:33:30 +0900

    Time: 1376 ms (24.67%), Space: 15.8 MB (36.92%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -4,17 +4,19 @@ class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
         ans = 0
         cnt = 0
-        alp = {j : i for i,j in enumerate('abcdefghijklmnopqrstuvwxyz')}
+        a = ord('a')
         dq = [deque() for _ in range(26)]
         for i in range(len(words)):
-            dq[alp[words[i][0]]].append([i,0])
+            c = ord(words[i][0]) - a
+            dq[c].append([i,0])
         for c in s:
-            x = alp[c]
-            for _ in range(len(dq[x])):
+            x = ord(c) - a
+            sz = len(dq[x])
+            for _ in range(sz):
                 i, j = dq[x].popleft()
                 j += 1
                 if len(words[i]) == j:
                     ans += 1
                 else:
-                    dq[alp[words[i][j]]].append([i,j])
+                    dq[ord(words[i][j]) - a].append([i,j])
         return ans
