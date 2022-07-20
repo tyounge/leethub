5c04f44d356f3713e4aa0128875dfbc5a800d5cc
TAEYOUNG(Teo) SEO <1004tyoung@gmail.com>
2022-07-20 23:00:31 +0900

    Time: 1161 ms (29.20%), Space: 16 MB (30.15%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -6,18 +6,19 @@ class Solution:
         cnt = 0
         alp = {j : i for i,j in enumerate('abcdefghijklmnopqrstuvwxyz')}
         idx = [0 for _ in range(len(words))]
+        list_idx = [0 for _ in range(26)]
         dq = [[] for _ in range(26)]
         for i in range(len(words)):
             dq[alp[words[i][0]]].append(i)
         for c in s:
             x = alp[c]
-            y = dq[x][:]
-            dq[x] = []
-            for i in y:
+            sz = len(dq[x])
+            for i in dq[x][list_idx[x]:sz]:
                 idx[i] += 1
                 j = idx[i]
                 if len(words[i]) == j:
                     ans += 1
                 else:
                     dq[alp[words[i][j]]].append(i)
+            list_idx[x] = sz
         return ans
