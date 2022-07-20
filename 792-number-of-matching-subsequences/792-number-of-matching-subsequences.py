640a13cd8dee44015c762696f38d66bb38ccaa95
TAEYOUNG(Teo) SEO <1004tyoung@gmail.com>
2022-07-20 22:17:02 +0900

    Time: 1180 ms (28.88%), Space: 15.8 MB (36.92%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -7,12 +7,10 @@ class Solution:
         alp = {j : i for i,j in enumerate('abcdefghijklmnopqrstuvwxyz')}
         dq = [deque() for _ in range(26)]
         for i in range(len(words)):
-            c = alp[words[i][0]]
-            dq[c].append([i,0])
+            dq[alp[words[i][0]]].append([i,0])
         for c in s:
             x = alp[c]
-            sz = len(dq[x])
-            for _ in range(sz):
+            for _ in range(len(dq[x])):
                 i, j = dq[x].popleft()
                 j += 1
                 if len(words[i]) == j:
